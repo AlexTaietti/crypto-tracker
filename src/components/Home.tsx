08a34a85f325294b329f56e015bdaa6396f4ffc8
AlexTaietti <taiettialex@yahoo.it>
2021-04-26 18:13:20 +0100

    🚀 IMPROVEMENT: create the first component files

---
+++ src/components/Home.tsx
@@ -0,0 +1,3 @@
+import { Header } from './Header';
+
+export const Home: React.FC = () => null;
---
+++ src/components/Tracker.tsx
@@ -0,0 +1,11 @@
+import { Home } from './Home';
+
+export const Tracker: React.FC = () => {
+
+   return (
+      <Home>
+
+      </Home>
+   )
+
+};
